fd60ce4c736aab77982829639a0860632a930774
Ruben Afonso <rbfrancos@gmail.com>
2019-07-26 09:20:48 +0100

    Add allow-insecure-unlock to use accounts from truffle

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -2,7 +2,7 @@
 module.exports = {
   networks: {
     development: {
-     host: "172.19.0.4",     // Localhost (default: none)
+     host: "172.18.0.3",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "500",       // Any network (default: none)
     },
